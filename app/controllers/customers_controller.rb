64b55d0c518819b3f104668625a5d8327e74d90f
alberto <alberto.email@gmail.com>
2016-01-29 00:41:48 +0100

    Connecting to and exploring API

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,20 @@
+require 'faraday'
+require 'faraday_middleware'
+
 class CustomersController < ApplicationController
   def index
+    key = Rails.application.secrets.besepa_api_key
+    url = "https://sandbox.besepa.com/api/1/"
+    @client = Faraday.new(:url => url, :headers => {:authorization => "Bearer #{key}"}) do |faraday|
+      faraday.request :json
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      faraday.response :json
+    end
+
+    response = @client.get("customers")
+    @customers = response.body["response"]
   end
 
   def show
