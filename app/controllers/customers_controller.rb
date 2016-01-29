9ffc0b92544d7599a3c8d180ffc27e33bfa964ac
alberto <alberto.email@gmail.com>
2016-01-29 02:48:40 +0100

    Refactoring

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,19 +2,25 @@ require 'faraday'
 require 'faraday_middleware'
 
 class CustomersController < ApplicationController
-  def index
-    key = Rails.application.secrets.besepa_api_key
-    url = "https://sandbox.besepa.com/api/1/"
-    @client = Faraday.new(:url => url, :headers => {:authorization => "Bearer #{key}"}) do |faraday|
-      faraday.request :json
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-      faraday.response :json
-    end
+  def besepa_client
+      key = Rails.application.secrets.besepa_api_key
+      url = "https://sandbox.besepa.com/api/1/"
+      Faraday.new(:url => url, :headers => {:authorization => "Bearer #{key}"}) do |faraday|
+        faraday.request :json
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+        faraday.response :json
+      end
+  end
 
-    response = @client.get("customers")
-    @customers = response.body["response"]
+  def get_customers
+      response = besepa_client.get("customers")
+      response.body["response"]
+  end
+  
+  def index
+    @customers = get_customers
   end
 
   def show
