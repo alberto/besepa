e5b8a00f79e6ec854fe60b85b261f737915c2e0b
alberto <alberto.email@gmail.com>
2016-01-29 17:31:39 +0100

    Edit customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,16 @@
 require 'faraday'
 require 'faraday_middleware'
 
+module Besepa
+  module Resources
+    class Customer < OpenStruct
+      def initialize(params)
+        super
+      end
+    end
+  end
+end
+
 class CustomersController < ApplicationController
   def index
     @customers = get_customers
@@ -14,14 +24,18 @@ class CustomersController < ApplicationController
   end
 
   def create
-      create_customer(customer_params)
-      redirect_to customers_path, notice: 'Cliente creado correctamente.'
+    create_customer(customer_params)
+    redirect_to customers_path, notice: 'Cliente creado correctamente.'
   end
 
   def edit
+    @customer = Besepa::Resources::Customer.new(get_customer(params[:id]))
   end
 
   def update
+    id = params[:id]
+    update_customer(customer_params, id)
+    redirect_to customers_path, notice: 'Cliente creado correctamente.'
   end
 
   def destroy
@@ -29,32 +43,36 @@ class CustomersController < ApplicationController
 
   private
   def customer_params
-      params.require(:customer).permit(:name, :taxid, :reference, :contact_name, :contact_email, :contact_phone, :address_street, :address_city, :address_postalcode, :address_state, :address_country)
+    params.require(:customer).permit(:name, :taxid, :reference, :contact_name, :contact_email, :contact_phone, :address_street, :address_city, :address_postalcode, :address_state, :address_country)
   end
 
   def besepa_client
-      key = Rails.application.secrets.besepa_api_key
-      url = "https://sandbox.besepa.com/api/1/"
-      Faraday.new(:url => url, :headers => {:authorization => "Bearer #{key}"}) do |faraday|
-        faraday.request :json
-        faraday.request  :url_encoded             # form-encode POST params
-        faraday.response :logger                  # log requests to STDOUT
-        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-        faraday.response :json
-      end
+    key = Rails.application.secrets.besepa_api_key
+    url = "https://sandbox.besepa.com/api/1/"
+    Faraday.new(:url => url, :headers => {:authorization => "Bearer #{key}"}) do |faraday|
+      faraday.request :json
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      faraday.response :json
+    end
   end
 
   def get_customers
-      response = besepa_client.get("customers")
-      response.body["response"]
+    response = besepa_client.get("customers")
+    response.body["response"]
   end
 
   def get_customer(id)
-      response = besepa_client.get("customers/#{id}")
-      response.body["response"]
+    response = besepa_client.get("customers/#{id}")
+    ActiveSupport::HashWithIndifferentAccess.new(response.body["response"])
   end
 
   def create_customer(customer)
-      response = besepa_client.post("customers", {customer: customer})
+    response = besepa_client.post("customers", {customer: customer})
+  end
+
+  def update_customer(customer, id)
+    response = besepa_client.put("customers/#{id}", {customer: customer})
   end
 end
